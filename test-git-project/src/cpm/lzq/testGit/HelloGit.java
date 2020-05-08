3b32c62edeb856a876a7cd6b0309781157e43518
LZQ <1440929767@qq.com>
2020-05-08 22:31:50 +0800

    第一次提交代码到Git本地仓库

---
+++ test-git-project/src/cpm/lzq/testGit/HelloGit.java
@@ -0,0 +1,14 @@
+package cpm.lzq.testGit;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("测试idea中使用git");
+        System.out.println("测试idea中使用git");
+        System.out.println("测试idea中使用git");
+        System.out.println("测试idea中使用git");
+        System.out.println("测试idea中使用git");
+
+
+    }
+}
